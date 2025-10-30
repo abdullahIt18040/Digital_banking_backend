e0407d075d5a399700154c719adb1ffc56f254b7
Md. Abdullah Al Mamun <58597051+abdullahIt18040@users.noreply.github.com>
2025-10-30 08:31:06 +0600

    add course and category table

---
+++ src/main/java/com/sil/digitalbankingbackend/entities/Category.java
@@ -0,0 +1,23 @@
+package com.sil.digitalbankingbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category",cascade = CascadeType.ALL)
+    private List<Course> course;
+
+}
---
+++ src/main/java/com/sil/digitalbankingbackend/entities/Course.java
@@ -0,0 +1,21 @@
+package com.sil.digitalbankingbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+   private String title;
+   @ManyToOne
+   @JoinColumn(name = "category_id")
+   private Category category;
+
+}
